56d9d145225dd4ecd974dc32ddbbca6cf8b13e5c
Maxsetima <kipsangk98@gmail.com>
2025-03-23 02:18:27 +0300

    Update PostForm to use TagWidget for the tags field

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,8 +1,8 @@
 from django import forms
-from .models import Post
-from .models import Comment
+from .models import Post, Comment
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from taggit.forms import TagWidget  # Import TagWidget from django-taggit
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -17,15 +17,16 @@ class CustomUserCreationForm(UserCreationForm):
         if commit:
             user.save()
         return user
+
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content', 'tags']  # Include tags field
         widgets = {
-            'tags': forms.CheckboxSelectMultiple(),  # Optionally, use a widget for easier tag selection
+            'tags': TagWidget(),  # Use TagWidget for easier tag input management
         }
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
-        fields = ['content']        
+        fields = ['content']
