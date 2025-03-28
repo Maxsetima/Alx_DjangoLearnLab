d9cd149a95a1d4a7dd8ae7fd0e344146b656a2b7
Maxsetima <kipsangk98@gmail.com>
2025-03-28 23:30:40 +0300

    Fix UserSerializer import and update views

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -1,16 +1,27 @@
+from django.contrib.auth.models import BaseUserManager
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, email=None, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        user = self.model(username=username, email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)  # This hashes the password
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(username, email, password, **extra_fields)
+
 class CustomUser(AbstractUser):
-    bio = models.TextField(blank=True, null=True)  # Optional bio for the user
-    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)  # Profile picture field
-    # followers: Many-to-Many relationship with itself (not symmetrical)
-    followers = models.ManyToManyField(
-        'self', 
-        symmetrical=False,  # Not symmetrical, as one user can follow another without being followed back
-        blank=True,  # Allow users to have no followers
-        related_name='following',  # Allows access to the list of users the current user is following
-    )
+    bio = models.TextField(blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
+    followers = models.ManyToManyField('self', symmetrical=False, blank=True, related_name='following')
+
+    objects = CustomUserManager()  # Add this line to use the custom user manager
 
     def __str__(self):
-        return self.username  # Return the username when the user instance is printed
+        return self.username
